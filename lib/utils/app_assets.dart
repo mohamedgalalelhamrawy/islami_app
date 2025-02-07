63988a5dd14f8d3d2d2390b7d43f58df45a788de
mohamedgalalelhamrawy <mo.galal200012@gmail.com>
2025-02-07 14:47:57 +0200

    create basic project (app_assets , app_styles , app_colors)

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,10 @@
+abstract class AppAssets {
+  static const String backgroundImage = "assets/images/default_bg.png";
+  static const String darkBackgroundImage = "assets/images/dark_bg.png";
+  static const String hadethLogo = "assets/images/hadeth_logo.png";
+  static const String iconHadeth = "assets/images/icon_hadeth.png";
+  static const String iconQuran = "assets/images/icon_quran.png";
+  static const String iconRadio = "assets/images/icon_radio.png";
+  static const String iconSebha = "assets/images/icon_sebha.png";
+  static const String quranLogo = "assets/images/qur2an_screen_logo.png";
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,8 @@
+ import 'package:flutter/material.dart';
+
+abstract class AppColors {
+    static const primary = Color(0xff94774D);
+    static const accent = Color(0xffDCDBDB);
+    static const whiteColor = Colors.white;
+    static const transparentColor = Colors.transparent;
+ }
